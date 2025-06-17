cc7d606a3dbab974deda05b3bf2079c38bb84632
keiko233 <i@elaina.moe>
2025-06-17 06:22:22 +0000

    fix: bc build

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,4 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
 
-export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache,
-});
+export default defineCloudflareConfig({});
